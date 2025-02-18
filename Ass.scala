3728554cac5ab9a761b306d42dec17daac7e637f
sandhya-80 <115801078+sandhya-80@users.noreply.github.com>
2025-02-18 08:44:34 +0530

    Update Ass.scala

--- Ass.scala
+++ Ass.scala
@@ -1,21 +1,24 @@
 // Assignment 1: Classes and Objects
 
 // Define a Person class
-class Person(val name: String, val age: Int) {
+class Person(val name: String, val age: Int) 
+{
   def greet(): String = s"Hello, my name is $name and I am $age years old."
 }
 
 // Companion Object for Person
-object Person {
+object Person 
+{
   def apply(name: String, age: Int): Person = new Person(name, age)
 }
 
 // Testing Classes and Objects
-object PersonTest extends App {
+object PersonTest extends App 
+{
   // Instantiate Objects
   val person1 = new Person("Alice", 25)
   val person2 = Person("Bob", 30) // Using companion object apply method
-  
+  val person3 = new Person("Alice", 25)
   // Invoke the greet method
   println(person1.greet())
   println(person2.greet())
